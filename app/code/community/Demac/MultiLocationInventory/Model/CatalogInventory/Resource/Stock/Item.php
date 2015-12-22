69a14080c9295d6e6472a3adbf1ed065cf31420f
Wilhelm Ellmann <wilhelm@fontera.com>
2015-12-22 11:27:01 +0200

    cataloginventory_stock_item seems empty for simple products, in this case overriding these values with default values

--- app/code/community/Demac/MultiLocationInventory/Model/CatalogInventory/Resource/Stock/Item.php
+++ app/code/community/Demac/MultiLocationInventory/Model/CatalogInventory/Resource/Stock/Item.php
@@ -16,11 +16,43 @@ class Demac_MultiLocationInventory_Model_CatalogInventory_Resource_Stock_Item ex
      */
     private function _loadByProductAndStoreId(Mage_CatalogInventory_Model_Stock_Item $item, $productId, $storeId)
     {
+        $arrDefaultData = array(
+            'product_id'                  => $productId,
+            'stock_id'                    => 0,
+            'qty'                         => 0.0000,
+            'min_qty'                     => 0.0000,
+            'use_config_min_qty'          => 1,
+            'is_qty_decimal'              => 0,
+            'backorders'                  => 0,
+            'use_config_backorders'       => 1,
+            'min_sale_qty'                => 1.0000,
+            'use_config_min_sale_qty'     => 1,
+            'max_sale_qty'                => 0.0000,
+            'use_config_max_sale_qty'     => 1,
+            'is_in_stock'                 => 0,
+            'low_stock_date'              => null,
+            'notify_stock_qty'            => null,
+            'use_config_notify_stock_qty' => 1,
+            'manage_stock'                => 0,
+            'use_config_manage_stock'     => 1,
+            'stock_status_changed_auto'   => 0,
+            'use_config_qty_increments'   => 1,
+            'qty_increments'              => 0.0000,
+            'use_config_enable_qty_inc'   => 1,
+            'enable_qty_increments'       => 0,
+            'is_decimal_divided'          => 0
+        );
+
+        // Only configurable products seem to be in the
         $select = $this->_getLoadSelect('product_id', $productId, $item)
             ->where('stock_id = :stock_id');
 
         $data   = $this->_getReadAdapter()
             ->fetchRow($select, array(':stock_id' => $item->getStockId()));
+
+        if(empty($data)) {
+            $data = $arrDefaultData;
+        }
 
         $stockStatusCollection = Mage::getModel('demac_multilocationinventory/stock_status_index')
             ->getCollection()
@@ -30,9 +62,9 @@ class Demac_MultiLocationInventory_Model_CatalogInventory_Resource_Stock_Item ex
         $stockStatus = $stockStatusCollection->getFirstItem();
 
         if ($data && $stockStatus->getId()) {
-            $data['qty']         = $stockStatus->getQty();
-            $data['backorders']  = $stockStatus->getBackorders();
-            $data['is_in_stock'] = $stockStatus->getIsInStock();
+            $data['qty']          = $stockStatus->getQty();
+            $data['backorders']   = $stockStatus->getBackorders();
+            $data['is_in_stock']  = $stockStatus->getIsInStock();
             $data['manage_stock'] = $stockStatus->getManageStock();
             //@TODO support use_config_backorders
             //$data['use_config_backorders'] = 1;//override...
